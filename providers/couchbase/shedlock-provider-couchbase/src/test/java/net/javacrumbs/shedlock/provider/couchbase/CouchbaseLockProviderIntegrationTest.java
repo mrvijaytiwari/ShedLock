f59eb90d30fa37235118d95ef2af7523b942ac6a
Lukas Krecan <lukas.krecan@adleritech.net>
2018-09-22 21:04:41 +0200

    Using instant

--- providers/couchbase/shedlock-provider-couchbase/src/test/java/net/javacrumbs/shedlock/provider/couchbase/CouchbaseLockProviderIntegrationTest.java
+++ providers/couchbase/shedlock-provider-couchbase/src/test/java/net/javacrumbs/shedlock/provider/couchbase/CouchbaseLockProviderIntegrationTest.java
@@ -5,16 +5,22 @@ import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.query.N1qlParams;
+import com.couchbase.client.java.query.consistency.ScanConsistency;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.test.support.AbstractLockProviderIntegrationTest;
-import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 import java.time.LocalDateTime;
 
-import static net.javacrumbs.shedlock.provider.couchbase.CouchbaseLockProvider.*;
+import static com.couchbase.client.java.query.N1qlQuery.simple;
+import static net.javacrumbs.shedlock.provider.couchbase.CouchbaseLockProvider.LOCKED_AT;
+import static net.javacrumbs.shedlock.provider.couchbase.CouchbaseLockProvider.LOCKED_BY;
+import static net.javacrumbs.shedlock.provider.couchbase.CouchbaseLockProvider.LOCK_UNTIL;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CouchbaseLockProviderIntegrationTest extends AbstractLockProviderIntegrationTest{
 
@@ -42,6 +48,11 @@ public class CouchbaseLockProviderIntegrationTest extends AbstractLockProviderIn
         lockProvider = new CouchbaseLockProvider(bucket);
     }
 
+    @After
+    public void clear() {
+        bucket.query(simple(String.format("DELETE FROM `%s`", bucket.name()), N1qlParams.build().consistency(ScanConsistency.STATEMENT_PLUS)));
+    }
+
     @Override
     protected LockProvider getLockProvider() {
         return lockProvider;
@@ -50,18 +61,18 @@ public class CouchbaseLockProviderIntegrationTest extends AbstractLockProviderIn
     @Override
     public void assertUnlocked(String lockName) {
         JsonDocument lockDocument = bucket.get(lockName);
-        Assertions.assertThat(LocalDateTime.parse((String)lockDocument.content().get(LOCK_UNTIL)).isBefore(LocalDateTime.now()));
-        Assertions.assertThat(LocalDateTime.parse((String)lockDocument.content().get(LOCKED_AT)).isBefore(LocalDateTime.now()));
-        Assertions.assertThat(!((String) lockDocument.content().get(LOCKED_BY)).isEmpty());
+        assertThat(LocalDateTime.parse((String) lockDocument.content().get(LOCK_UNTIL))).isBefore(LocalDateTime.now());
+        assertThat(LocalDateTime.parse((String) lockDocument.content().get(LOCKED_AT))).isBefore(LocalDateTime.now());
+        assertThat(lockDocument.content().get(LOCKED_BY)).asString().isNotEmpty();
     }
 
     @Override
     public void assertLocked(String lockName) {
 
         JsonDocument lockDocument = bucket.get(lockName);
-        Assertions.assertThat(LocalDateTime.parse((String) lockDocument.content().get(LOCK_UNTIL)).isAfter(LocalDateTime.now()));
-        Assertions.assertThat(LocalDateTime.parse((String) lockDocument.content().get(LOCKED_AT)).isBefore(LocalDateTime.now()));
-        Assertions.assertThat(!((String) lockDocument.content().get(LOCKED_BY)).isEmpty());
+        assertThat(LocalDateTime.parse((String) lockDocument.content().get(LOCK_UNTIL))).isAfter(LocalDateTime.now());
+        assertThat(LocalDateTime.parse((String) lockDocument.content().get(LOCKED_AT))).isBefore(LocalDateTime.now());
+        assertThat(lockDocument.content().get(LOCKED_BY)).asString().isNotEmpty();
 
     }
 
@@ -74,10 +85,9 @@ public class CouchbaseLockProviderIntegrationTest extends AbstractLockProviderIn
     }
 
     private Bucket getBucket(Cluster cluster) {
-        cluster.authenticate(BUCKET_NAME, BUCKET_NAME);
-        Bucket bucket = cluster.openBucket(BUCKET_NAME);
+        cluster.authenticate("Administrator", "123456");
 
-        return bucket;
+        return cluster.openBucket(BUCKET_NAME);
     }
 
 }
